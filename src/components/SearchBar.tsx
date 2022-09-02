758d405da325f4a446f1584a43560e5ebcb87ac4
omarelmoghazy <omarelmoghazy1@gmail.com>
2022-09-02 16:33:22 +0200

    Create a component for the Search Bar

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import { Feather } from '@expo/vector-icons';
+
+const SearchBarContainer = styled.View`
+  justify-content: flex-end;
+  align-items: center;
+  padding: 20px 0;
+`;
+
+const Bar = styled.View`
+  width: 85%;
+  flex-direction: row;
+  background: rgba(255, 255, 255, 0.3);
+  border-radius: 5px;
+  padding: 10px;
+`;
+
+const BarInput = styled.TextInput.attrs({
+  placeholder: 'Search',
+  placeholderTextColor: '#fff',
+})`
+  flex: 1;
+  margin-left: 10px;
+  font-size: 16px;
+  font-family: 'PoppinsRegular';
+  color: #fff;
+`;
+
+const SearchBar = () => {
+  return (
+    <SearchBarContainer>
+      <Bar>
+        <Feather name="search" size={20} color="white" />
+        <BarInput />
+      </Bar>
+    </SearchBarContainer>
+  );
+};
+
+export default SearchBar;
