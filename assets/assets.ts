a8a8e1d2a0c82cb3dca0c1ee56386d9b76583c10
omarelmoghazy <omarelmoghazy1@gmail.com>
2022-09-06 16:28:39 +0200

    Added extra image assets

--- assets/assets.ts
+++ assets/assets.ts
@@ -1,1 +1,3 @@
-export { default as SplashScreenLogo } from "./images/Nasdaq-logo-title.png";
+export { default as SplashScreenLogo } from './images/Nasdaq-logo-title.png';
+export { default as UnavailableLogo } from './images/unavailable-icon.jpg';
+export { default as ErrorImageJPG } from './images/ErrorPageImage.png';
