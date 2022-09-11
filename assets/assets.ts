0db48590a3707582940aa9124a7cf0183eb9086b
omarelmoghazy <omarelmoghazy1@gmail.com>
2022-09-11 14:36:00 +0200

    Remove unused assets

--- assets/assets.ts
+++ assets/assets.ts
@@ -1,3 +1,2 @@
 export { default as SplashScreenLogo } from './images/Nasdaq-logo-title.png';
-export { default as UnavailableLogo } from './images/unavailable-icon.jpg';
 export { default as ErrorImageJPG } from './images/ErrorPageImage.png';
